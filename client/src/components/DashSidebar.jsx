fdfc3c5ff113a3edba86c114cbc46088b7bfa456
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-03-09 13:57:52 +0000

    #

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -44,7 +44,7 @@ export default function DashSidebar() {
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
         <Sidebar.ItemGroup className='flex flex-col gap-1'>
-          {currentUser && currentUser.isAdmin && (
+        
             <Link to='/dashboard?tab=dash'>
               <Sidebar.Item
                 active={tab === 'dash' || !tab}
@@ -54,19 +54,19 @@ export default function DashSidebar() {
                 Dashboard
               </Sidebar.Item>
             </Link>
-          )}
+        
           <Link to='/dashboard?tab=profile'>
             <Sidebar.Item
               active={tab === 'profile'}
               icon={HiUser}
-              label={currentUser.isAdmin ? 'Admin' : 'User'}
+              
               labelColor='dark'
               as='div'
             >
               Profile
             </Sidebar.Item>
           </Link>
-          {currentUser.isAdmin && (
+         
             <Link to='/dashboard?tab=posts'>
               <Sidebar.Item
                 active={tab === 'posts'}
@@ -76,8 +76,8 @@ export default function DashSidebar() {
                 Posts
               </Sidebar.Item>
             </Link>
-          )}
-          {currentUser.isAdmin && (
+          
+         
             <>
               <Link to='/dashboard?tab=users'>
                 <Sidebar.Item
@@ -98,7 +98,7 @@ export default function DashSidebar() {
                 </Sidebar.Item>
               </Link>
             </>
-          )}
+          
           <Sidebar.Item
             icon={HiArrowSmRight}
             className='cursor-pointer'

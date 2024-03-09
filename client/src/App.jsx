706a18b5aff5b58204943c7cefd974149da5d11d
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-03-09 12:22:51 +0000

    #

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,9 +27,9 @@ export default function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/search' element={<Search />} />
-        <Route element={<PrivateRoute />}>
+       
           <Route path='/dashboard' element={<Dashboard />} />
-        </Route>
+        
         <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
           <Route path='/update-post/:postId' element={<UpdatePost />} />
